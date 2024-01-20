c3bfc0f5330accb2cb944d4f8eb4ef1edf90ee19
Maks Dubinkin <maksdubinkin95@gmail.com>
2024-01-20 15:10:48 +0500

    header create

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Header from "./components/Header/index";
 function App() {
   return (
     <div className="App">
-      <Header></Header>
+      {/* <Header></Header> */}
     </div>
   );
 }
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,12 +1,6 @@
 const Header = () => {
   return (
-    <header>
-      <img src="./img/logo.png"></img>
-      <div>
-        <p>Фрукты Бонифация</p>
-        <p>Только спелые фрукты</p>
-      </div>
-    </header>
+    <p>ds</p>
   );
 };
 
